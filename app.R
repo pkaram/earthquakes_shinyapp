b91527f930aa995c6bac520b2b6d7b841d0ce929
pk778 <karamerosp+github1@gmail.com>
2024-02-23 15:46:34 +0100

    refactor for docker image

--- app.R
+++ app.R
@@ -1,8 +1,57 @@
-#interactive map, which loads data of earthquakes over the period 1900-2009
-#Option for magnitude/Option for date Range
-#Under the map is presented the list of earthquakes for options selected
+library(gdata)
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(readxl)
 
-function(input, output) {
+data_temp<-read_xls("/home/earthquakes-shinyapp/1900_2009_mkk.xls",sheet=1)
+data_temp<-data_temp %>% mutate(date_time=paste(YEAR,MONTH,DAY,HOUR,MIN,SEC))
+data_temp<-data_temp %>% mutate(date_time=as.POSIXct(date_time,format="%Y %m %d %H %M %S"))
+data_temp<-data_temp %>% select(Date=date_time,
+                      Latitude=LAT,
+                      Longitude=LON,
+                      Depth=DEP,
+                      Magnitude_Ms=Ms,
+                      Magnitude_Mw=Mw
+                      )
+
+ui <- fluidPage(
+  headerPanel('Earthquake Catalogue for Greece and adjacent areas 1900-2009'),
+  sidebarPanel(
+    radioButtons('type', 'Select Earthquake Type',
+                 c('Surface Wave Magnitude'="Ms",
+                   'Moment Magnitude'="Mw"),
+                 selected='Ms'),
+    selectInput("rel","",choices = as.list(c(">"=">",
+                                             ">="='>=',
+                                             "<"='<',
+                                             "<="='<='
+    )),selected = ">"),
+    
+    radioButtons('mag', ' ',
+                 c("7"=7,
+                   "6"=6,
+                   "5"=5,
+                   "4"=4
+                 ),
+                 selected=7),
+    tags$hr(),
+    checkboxInput('all', 'All Earthquakes For Type Selected', FALSE),
+    tags$hr(),
+    dateRangeInput("rangedate","Select Date Range",start = "1900-01-01", end = "2009-12-31",format = "yyyy-mm-dd",separator = '-'),
+    tags$hr(),
+    #open source of data in new tab
+    p(a("Source", href = "http://www.gein.noa.gr/en/seismicity/earthquake-catalogs", target = "_blank"))
+  ),
+  
+  mainPanel(
+    leafletOutput('myMap'),
+    tags$hr(),
+    DT::dataTableOutput("eqlist")
+  )
+)
+
+server <- function(input, output) {
   #data based on selection of radiobutton and selectinput
   data_filter<-reactive({
     
@@ -82,3 +131,7 @@ function(input, output) {
   })
   
 }
+
+
+
+shinyApp(ui = ui, server = server)
